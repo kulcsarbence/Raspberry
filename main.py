57cdf7b6c81a717792d188d6a48c37b280aefa49
kulcsarbence <bance98@gmail.com>
2019-09-27 14:45:52 +0200

    aha

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ GPIO.setwarnings(False)
 rdr = RFID()
 
 uidPath = "acceptedUID.txt"
+parkedPath = "parkedUID.txt"
 line_count = sum(1 for line in open(uidPath))
+global p_line_count
 acceptedUID = [[0 for x in range(5)] for y in range(line_count)]
-
+global p_acceptedUID
+p_line_count = sum(1 for line in open(parkedPath))
+p_acceptedUID = [[0 for x in range(5)] for y in range(p_line_count)]
 #Az ultrahang szenzor fuggvenyei kovetkeznek:
 
 trigPin = 35
@@ -51,6 +55,32 @@ def isThereACar():
 
 #Vege az ultrahang szenzor fuggvenyeinek
 
+def p_getAcceptedUID():
+    p_acceptedUID = [[0 for x in range(5)] for y in range(p_line_count)]
+    file2 = open(parkedPath, "r")
+    for i in range(0, p_line_count, 1):
+        string = file2.readline()
+        p_acceptedUID[i] = [x.strip() for x in string.split(',')]
+        for j in range(0,5,1):
+            print(p_acceptedUID[i][j])
+            p_acceptedUID[i][j] = int(p_acceptedUID[i][j])
+    file2.close()
+    return p_acceptedUID
+
+def p_checkUIDmatch(current_uid):
+    accepted = p_getAcceptedUID()
+    tmp = True
+    toReturn = False
+    for i in range(0, p_line_count, 1):
+        tmp = True
+        for j in range(0, 5, 1):
+            if current_uid[j]!=accepted[i][j]:
+                tmp = False
+        if tmp:
+            toReturn = True
+    return toReturn
+
+
 def getAcceptedUID():
     file = open(uidPath, "r")
     for i in range(0, line_count, 1):
@@ -87,20 +117,46 @@ def getUID():
 
 
 def main():
+    global p_line_count
     try:
         while True:
+            print(p_line_count)
             print("Elindult a while true")
             uidd = getUID()
-            if checkUIDmatch(uidd):
-                print("Belepes engedelyezve!")
-                while(not isThereACar()):
-                    print("Varakozunk")
-                print("Auto megerkezett")
+            if p_checkUIDmatch(uidd):
+                c = uidd
+                print("Viszontlatasra")
+                with open(parkedPath, "r") as f:
+                    lines = f.readlines()
+                with open(parkedPath, "w") as f:
+                    for line in lines:
+                        if line.strip("\n") != str(c[0])+","+str(c[1])+","+str(c[2])+","+str(c[3])+","+str(c[4]):
+                            f.write(line)
+                p_line_count = sum(1 for line in open(parkedPath, "r"))
                 time.sleep(sleepInterval)
-                print("Lecsukjuk a kaput")
             else:
-                print("elszartuk")
+                if checkUIDmatch(uidd):
+                    c = uidd
+                    print("Belepes engedelyezve!")
+                    File3 = open(parkedPath, "a+")
+                    print(c[0])
+                    File3.write(str(c[0])+","+str(c[1])+","+str(c[2])+","+str(c[3])+","+str(c[4])+"\n")
+                    File3.close()
+                    File4 = open(parkedPath, "r")
+                    print(File4.readlines())
+                    File4.close()
+                    p_line_count = sum(1 for line in open(parkedPath, "r"))
+                    print(p_line_count)
+                    while(not isThereACar()):
+                        print("")
+                    print("Auto megerkezett")
+                    time.sleep(sleepInterval)
+                    print("Lecsukjuk a kaput")
+                else:
+                    print("A kartya nincs a rendszerben.")
     except KeyboardInterrupt:
+        with open(parkedPath, "w") as f:
+            f.write("")
         GPIO.cleanup()
 
 if __name__=="__main__":
