3f3fe6cd5f5a0712b99d2a3de9fdc52606cf7563
vbence121 <vbence121@gmail.com>
2019-10-02 15:13:44 +0200

    added: 	rledpin, gatpin, empty space handling, twitter functionality, blink led function and gate movement missing: 	leave gate movement?

--- main.py
+++ main.py
@@ -21,8 +21,13 @@ p_acceptedUID = [[0 for x in range(5)] for y in range(p_line_count)]
 trigPin = 35
 echoPin = 37
 gLedPin = 38
+rLedPin = 40    # red led pin
+gatePin = 39    # gate trigger pin
 max_dist = 220
 timeOut = max_dist*60
+
+totalSpaces = 100           #inicializaljuk a max szabad helyeket, az ures helyeket
+emptySpaces=totalSpaces
 
 GPIO.setup(trigPin, GPIO.OUT)
 GPIO.setup(echoPin, GPIO.IN)
@@ -32,11 +37,11 @@ def pulseTime(pin, level, timeOut):
     t0 = time.time()
     while(GPIO.input(pin) != level):
         if((time.time() - t0) > timeOut*0.000001):
-            return 0;
+            return 0
     t0 = time.time()
     while(GPIO.input(pin) == level):
         if((time.time() - t0) > timeOut*0.00001):
-            return 0;
+            return 0
     pulse = (time.time() - t0)
     return pulse
 
@@ -117,7 +122,7 @@ def getUID():
     rdr.cleanup()
     return toReturn
 
-def initTitter():
+def initTwitter():
     # Authenticate to Twitter
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -134,22 +139,40 @@ def tweetString(string):
     pi.update_status(string) # send out tweet to twitter
     return
 
+def blinkLed(led_pin):
+    GPIO.output(led_pin, GPIO.HIGH)  # a kapott pinen helyezkedo ledet haromszot egymas utan felvilantjuk
+    sleep(1/2)                       # varunk 1/2 mp-t
+    GPIO.output(led_pin, GPIO.LOW)
+    sleep(1/2)
+    GPIO.output(led_pin, GPIO.HIGH)
+    leep(1/2)
+    GPIO.output(led_pin, GPIO.LOW)
+    leep(1/2)
+    GPIO.output(led_pin, GPIO.HIGH)
+    leep(1/2)
+    GPIO.output(led_pin, GPIO.LOW)
+    return
+
+
 def main():
+    initTwitter()       #titter initial
+    tweetString("Megnyitottunk, a helyek szama: "+str(totalSpaces))     #ertesitjuk a felhasznalokat a rendszer indulasarol 
     global p_line_count
     try:
         while True:
-            totalSpaces = 100
-            emptySpaces=totalSpaces
-            tweetString("Megnyitottunk, a helyek szama: "+str(totalSpaces))
+            if emptySpaces == 0:                # ha a parkolo tele van
+                GPIO.output(rLedPin, GPIO.HIGH) #    a piros led szolidan vilagit, ezzel jelezve a felhasznalok szamara hogy nincs szabad hely
+            else:
+                GPIO.output(rLedPin, GPIO.LOW)  #    ellenkezo esetben a led kialszik
             print(p_line_count)
             print("Elindult a while true")
             uidd = getUID()
             if p_checkUIDmatch(uidd):
                 c = uidd
                 print("Viszontlatasra")
-                if(emptySpaces != totalSpaces):
+                if(emptySpaces != totalSpaces):             #biztositjuk hogy a szabad helyek erteke a vart hatarakon belul maradjon
                     emptySpaces += 1
-                    tweetString("Jelenleg a helyek szama: "+str(emptySpaces))
+                tweetString("Jelenleg a helyek szama: "+str(emptySpaces))
                 with open(parkedPath, "r") as f:
                     lines = f.readlines()
                 with open(parkedPath, "w") as f:
@@ -159,13 +182,14 @@ def main():
                 p_line_count = sum(1 for line in open(parkedPath, "r"))
                 time.sleep(sleepInterval)
             else:
-                if checkUIDmatch(uidd):
+                if checkUIDmatch(uidd) & emptySpaces != 0: #ha az uid az adatbazisban van ES van meg ures hely
                     c = uidd
                     print("Belepes engedelyezve!")
-                    if(emptySpaces != 0):
+                    if(emptySpaces != 0):                   #biztositjuk hogy a szabad helyek erteke a vart hatarakon belul maradjon
                         emptySpaces -= 1
-                        tweetString("Jelenleg a helyek szama: "+str(emptySpaces))
+                    tweetString("Jelenleg a helyek szama: "+str(emptySpaces))
                     GPIO.output(gLedPin, GPIO.HIGH)
+                    GPIO.output(gatePin, GPIO.HIGH) #aktivaljuk a gate pin-t
                     File3 = open(parkedPath, "a+")
                     print(c[0])
                     File3.write(str(c[0])+","+str(c[1])+","+str(c[2])+","+str(c[3])+","+str(c[4])+"\n")
@@ -175,14 +199,16 @@ def main():
                     File4.close()
                     p_line_count = sum(1 for line in open(parkedPath, "r"))
                     print(p_line_count)
-                    while(not isThereACar()):
+                    while(isThereACar()):
                         print("")
                     print("Auto megerkezett")
                     time.sleep(sleepInterval)
                     GPIO.output(gLedPin, GPIO.LOW)
                     print("Lecsukjuk a kaput")
+                    GPIO.output(gatePin, GPIO.LOW)
                 else:
                     print("A kartya nincs a rendszerben.")
+                    blinkLed(rLedPin)
     except KeyboardInterrupt:
         with open(parkedPath, "w") as f:
             f.write("")
